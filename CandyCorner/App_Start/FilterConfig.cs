007173ca57239af61b7e97fe6cb4c5d789e6b006
Abdullah Ibn Mannan <sabbir.mannan@gmail.com>
2016-12-21 13:30:34 +0600

    Initial Commit

---
+++ CandyCorner/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CandyCorner
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ CandyCorner/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CandyCorner.MvcApplication" Language="C#" %>
